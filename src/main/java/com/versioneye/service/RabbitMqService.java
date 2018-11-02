8dcd089e991467c55f1c00da1cc045ced77e1ad6
Stefan Pokorny <stefan.pokorny@eacg.de>
2018-11-02 17:13:42 +0100

    refactored MQ Session handling

--- src/main/java/com/versioneye/service/RabbitMqService.java
+++ src/main/java/com/versioneye/service/RabbitMqService.java
@@ -31,6 +31,7 @@ public class RabbitMqService {
         pooledConnectionFactory = new PooledConnectionFactory();
         pooledConnectionFactory.setConnectionFactory(connectionFactory);
         pooledConnectionFactory.setMaxConnections(10);
+        pooledConnectionFactory.start();
 
         isQueueInitialized = true;
     }
